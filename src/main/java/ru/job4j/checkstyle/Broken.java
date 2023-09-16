454ad0bccf3eb50c3d6d155f29053c8d74348343
Andrey <ham2188@mail.ru>
2023-09-16 10:31:14 +0300

    Репозиторий под проект Tracker - fix

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,21 +2,21 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     private int sizeOfEmpty = 10;
-    String name;
-    public String surname;
+    private String name;
+    private String surname;
     public static final String NEW_VALUE = "";
 
-    Broken() { }
+    public Broken() { }
 
-    void echo() {
+    public void echo() {
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g) {
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
     }
 }
