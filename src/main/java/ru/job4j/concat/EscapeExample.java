ccd0b77a58a07dfec58908707bfcb2acb8c52e15
Andrey <ham2188@mail.ru>
2023-09-22 19:36:27 +0300

    VarArgs

---
+++ src/main/java/ru/job4j/concat/EscapeExample.java
@@ -0,0 +1,12 @@
+package ru.job4j.concat;
+
+public class EscapeExample {
+    public static void main(String[] args) {
+        System.out.println("Я изучаю на \"Job4J\" экранирование символов");
+        System.out.println("C:\\projects\\job4j\\File.java");
+        System.out.print("Первая строка\nВторая строка");
+        System.out.println("Без отступа");
+        System.out.println("\tОдин отступ");
+        System.out.println("\t\tДва отступа");
+    }
+}
