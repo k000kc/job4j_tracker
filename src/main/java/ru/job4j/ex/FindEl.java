c222ff483c9a4340dd7d833d3f10b403583aa3ec
Andrey <ham2188@mail.ru>
2023-09-27 20:44:54 +0300

    множественный catch

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,10 +1,10 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int index = 0; index < value.length; index++) {
-            if (value[index] == key) {
+        for (int index = 0; index < values.length; index++) {
+            if (values[index] == key) {
                 rsl = index;
             }
         }
@@ -12,6 +12,22 @@ public class FindEl {
             throw new ElementNotFoundException("element not found");
         }
         return rsl;
+    }
+
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException  ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
